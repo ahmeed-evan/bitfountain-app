318fb807013e956b4d6f95a8c718406872712f5d
ahmeed-evan <ahmeed.3826@gmail.com>
2021-05-15 11:12:27 +0600

    project setup

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter, {RouteConfig} from 'vue-router'
+import Overview from '../views/Overview.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        name: 'Overview',
+        component: Overview
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ src/views/Overview.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div id="overview"></div>
+
+</template>
+
+<script lang="ts">
+
+import {Vue} from "vue-property-decorator";
+
+export default class Overview extends Vue {
+
+}
+
+</script>
